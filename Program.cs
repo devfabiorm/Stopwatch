dc939c790ea3f9f3a98214619821d3fc8f1928e3
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-05-18 19:09:18 -0300

    Changed method signature to make it more flexible

--- Program.cs
+++ Program.cs
@@ -2,17 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Start(6);
     }
 
-    static void Start()
+    static void Start(int time)
     {
-        int time = 10;
         int currentTime = 0;
 
         while (currentTime != time)
         {
+            Console.Clear();
             currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
         }
+
+        Console.Clear();
+        Console.WriteLine("Stopwatch finalizado");
     }
 }
